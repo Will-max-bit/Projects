c84466e46aed747bcfbce26a119e5968f7f57c28
Will-max-bit <whermann4@gmail.com>
2022-09-10 15:46:39 -0700

    project at acceptable place

---
+++ javaScript/vue/color-palette-generator/README.md
@@ -0,0 +1,66 @@
+# color-palette-generator
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+# Color Pallettizer
+
+
+## Tech/framework used
+<ul>
+<li>Vue 3</li>
+<li>Google Fonts</li>
+</ul>
+
+
+## Screenshots and demo
+![Alt text](https://github.com/Will-max-bit/Projects/blob/main/javaScript/vue/color-palette-generator/demo.JPG "Demo Screenshot")
+
+## Installation
+Simply Git clone the project and use yarn serve to run the project
+
+## References
+Photo by <a href="https://unsplash.com/@ninaluong?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nina Luong</a> on <a href="https://unsplash.com/s/photos/paints?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+## License
+
+MIT License
+
+Copyright (c) [2022] [Will-Max-bit]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- javaScript/vue/color-palette-generator/src/App.vue
+++ javaScript/vue/color-palette-generator/src/App.vue
@@ -3,10 +3,10 @@
     <div>
       <h1 :style="textColorCompliment">Color Palette Generator</h1>
       <div v-if="copied == 1">
-        <p class="status">Copied</p>
+        <p class="status" :style="textColorCompliment">Copied</p>
       </div>
       <div v-else-if="duplicateFlag == 1">
-        <p class="status">Palette already saved</p>
+        <p :style="textColorCompliment" class="status">Palette already saved</p>
       </div>
       <div v-else>
         <p :style="textColorCompliment" class="status">Welcome to the Palette Generator</p>
@@ -24,8 +24,8 @@
       >
         <div class="code" :style="{ backgroundColor: color }">
           <select name="color-code" id="color-code" v-model="userColorCode" multiple>
-            <option :value="color" @click="copySingleColor(color)">{{color}}</option>
-            <option value="rgb" @click="copySingleColor(this.rgbColors[index])">{{this.rgbColors[index]}}</option>
+            <option :style="{color: 'singleColorCompliment[index]', fontFamily: 'Pacifico'}" :value="color" @click="copySingleColor(color)"><p class="font"> {{color}}</p></option>
+            <option :style="{color: 'singleColorCompliment[index]', fontFamily: 'Pacifico'}" @click="copySingleColor(this.rgbColors[index])"><p class="font">rgb{{this.rgbColors[index]}}</p></option>
           </select>
         </div>
       </div>
@@ -44,8 +44,9 @@
             v-for="(paletteColor, idx) in palette"
             :key="idx"
             :style="{ backgroundColor: paletteColor }"
+            class="saved-palettes"
           >
-            <p>
+            <p :style="textColorCompliment" >
               {{ paletteColor }}
             </p>
           </li>
@@ -61,32 +62,44 @@ export default {
   components: {},
   data() {
     return {
-      textColorCompliment: {
+      // styling for heading text
+      textColorCompliment: { 
         color: "",
       },
+      // styling for color code array text
+      singleColorCompliment: [], 
+      // background color gradient array
+      backgroundColors: [], 
+      // array of random colors in hex
       colors: [],
+      // array of color palettes saved by click event
       savedPaletts: [],
-      backgroundColors: [],
+      // array of current colors converted into rgb 
       rgbColors: [],
-      text: "",
-      pallettDuplicateFlag: 0,
+      // copied flag, 0 is false, when 1 is assigned copied appears in the dom
       copied: 0,
+      // duplicate flag, 0 is false, when 1 is assigned duplicate appears in the dom
       duplicateFlag: 0,
+      // v-model to color code picked, hex gets you a hex copy,rgb vice versa 
       userColorCode: '',
     };
   },
   methods: {
+    // Generates intial random colors and also creates background
     paletteGenerator() {
       this.colors = [];
       this.backgroundColors = [];
+      // random color loop
       for (let i = 0; i < 5; i++) {
         let randomColorHex = Math.floor(Math.random() * 16777215).toString(16);
         randomColorHex = "#" + randomColorHex;
         this.colors.push(randomColorHex);
       }
+      // assigns background colors and flips them so contrast is somewhat maintained
       this.backgroundColors = `linear-gradient(to left, ${this.colors[0]}, ${this.colors[1]}, ${this.colors[2]}, ${this.colors[3]}, ${this.colors[4]}`;
       this.rgbConverter(this.colors)
     },
+    // converts hex into rgb for easier text contrast responsiveness also triggers contrast functions to run
     rgbConverter(colors){
       let rgbArr = [];
             for (let color of colors) {
@@ -96,89 +109,132 @@ export default {
               let b = parseInt(color.substring(4, 6), 16);
               let singleColor = [r, g, b];
               rgbArr.push(singleColor);
+              // rgb colors is an array used to give the user multiple choices
               this.rgbColors.push(singleColor);
             }
       this.textColorContrast(rgbArr)
+      this.singleColorContrast(rgbArr)
     },
+    // most text is centered so header contrast is based off center color
     textColorContrast(rgbColors) {
-      // let rgbArr = [];
-      // for (let color of colors) {
-      //   color = color.replace("#", "");
-      //   let r = parseInt(color.substring(0, 2), 16);
-      //   let g = parseInt(color.substring(2, 4), 16);
-      //   let b = parseInt(color.substring(4, 6), 16);
-      //   let singleColor = [r, g, b];
-      //   rgbArr.push(singleColor);
-      //   this.rgbColors.push(singleColor);
-      // }
-      console.log(rgbColors, 'rgb colors')
-      let centerColor = rgbColors[2][1];
+      let centerColor = rgbColors[2][1]; 
       if (centerColor <= 100) {
         this.textColorCompliment.color = "rgba(235,235,235, 1)";
-      } else if (centerColor > 200) {
+      } else if (centerColor > 100) {
         this.textColorCompliment.color = "rgba(0,0,0, 1)";
       }
     },
+    // used to specifically target the text inside the palette blocks
+    // non functional at the moment however
+    singleColorContrast(rgbColors){
+      let colors = rgbColors
+      let singColorArr = []
+      if (colors <= 100) {
+        singColorArr.push("color: rgba(235,235,235, 1);")
+      } else if (colors > 200) {
+        singColorArr.push("color: rgba(0,0,0, 1);")
+      }
+      this.singleColorCompliment = singColorArr
+    },
+    // click event triggered to save current random palette
     savePalett() {
+      // stores global colors property as local variable
       let tempPalet = this.colors;
+      // the control for double saving a palette is checking to see if the last palette is equal to it.
       const checker = this.savedPaletts[this.savedPaletts.length - 1];
+      // controls size of saved palette array
       const paletteSize = this.savedPaletts.length;
+      // checking if palette is saved and adjusting flags based off that
       if (checker == this.colors) {
-        this.pallettDuplicateFlag = 0;
         this.duplicateFlag = 1;
+        // resetting the flag so the message disappears
         setTimeout(() => {
           this.duplicateFlag = 0;
         }, 2500);
       } else {
+        // if it is >= saved array will be reduced by one (first index) and new saved palette will be added 
         if (paletteSize >= 6) {
           this.savedPaletts.shift();
           this.savedPaletts.push(tempPalet);
-          this.pallettDuplicateFlag = 1;
         } else {
           this.savedPaletts.push(tempPalet);
-          this.pallettDuplicateFlag = 1;
         }
       }
     },
+    // controls set timeout for copied message
     timeOutHelper() {
       this.copied = 1;
       setTimeout(() => {
         this.copied = 0;
       }, 5000);
     },
+    // loading the palette at index clicked
     loadPalett(idx) {
       this.colors = this.savedPaletts[idx];
     },
+    // copies the current color array to clipboard
     copyCurrent() {
       let currentRaw = this.colors;
       let currentCopy = currentRaw.toString();
       navigator.clipboard.writeText(currentCopy);
       this.timeOutHelper();
     },
+    // copies the previous color array to clipboard
     savePreviousPalett(previousPalett) {
       let palletRaw = this.savedPaletts[previousPalett];
       let palletCopy = palletRaw.toString();
       navigator.clipboard.writeText(palletCopy);
       this.timeOutHelper();
     },
+    // copies single color
     copySingleColor(code) {
-      if (this.userColorCode != ''){
-        console.log('single color function from vue model')
-      }
       navigator.clipboard.writeText(code);
       this.timeOutHelper();
     },
   },
 };
 </script>
-
-
-
-
- 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Indie+Flower&family=Lobster&family=Pacifico&display=swap");
-/* fonts */
+/* layout */
+body {
+  margin: 0 auto;
+}
+main {
+  width: 100vw !important;
+  height: 100vh !important;
+}
+li {
+  list-style: none !important;
+}
+.box {
+    margin: 1%;
+    width: 10vw;
+    height: 20vw;
+    border: 2px black solid;
+    display: flex;
+}
+.colors {
+    display: flex;
+    justify-content: center;
+    margin-top: 1em;
+}
+.code {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    height: auto;
+    width: 100%;
+}
+.SavePaletts{
+  margin: 0;
+}
+.saved {
+  display: flex;
+  justify-content: center;
+}
+/* fonts and text layout */
 h1,
 h2 {
   font-family: "Pacifico", cursive;
@@ -191,6 +247,9 @@ h2 {
   font-size: 2.5rem;
   margin: 0 auto;
 }
+.font{
+  font-family: "Indie Flower", cursive !important;
+}
 p {
   font-family: "Indie Flower", cursive;
 }
@@ -198,44 +257,10 @@ p {
   font-size: 3rem;
   margin: 0 auto;
 }
-main {
-  width: 100vw !important;
-  height: 100vh !important;
-}
-li {
-  list-style: none !important;
-}
 li > p {
   color: aliceblue;
   font-size: 1.5em;
   margin: 0 auto;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.colors {
-  display: flex;
-  justify-content: center;
-  margin-top: 1em;
-}
-.box {
-  margin: 1%;
-  width: 10vw;
-  height: 20vw;
-  border: 2px black solid;
-}
-.code {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  /* background-color: black; */
-  height: auto;
-  width: 100%;
 }
 .code > p {
   text-align: center;
@@ -245,18 +270,30 @@ li > p {
 .code > input {
   text-align: center;
 }
-.code > button {
-  width: 50%;
-}
-.saved {
-  display: flex;
-  justify-content: center;
-}
 .saved > h4 {
   display: flex;
   flex-direction: column;
   align-self: center;
   flex: 0 0;
+}
+#color-code {
+    background: transparent;
+    border-style: none;
+    overflow: hidden;
+    text-align: center;
+}
+select > option:hover{
+font-size: 1.5rem;
+transition: 0.3s;
+padding: 2%;
+}
+select > option{
+  margin: 1rem;
+  font-size: 1.25rem;
+}
+/* inputs */
+.code > button {
+  width: 50%;
 }
 button {
   font-family: "Lobster", cursive;
@@ -265,5 +302,16 @@ button {
 }
 button:hover {
   box-shadow: 1px 3px 5px 4px rgba(0, 0, 0, 0.75);
+}
+/* individuals selections */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.saved-palettes{
+  border: solid 1px;
 }
 </style>
