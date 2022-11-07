20668c7253a4f8a1c056f7d36f7fb5992b989eb8
william hermann <will@williams-MacBook-Air.local>
2022-11-06 21:56:22 -0800

    first model written

--- Django/keepCloneProject/keepCloneApp/models.py
+++ Django/keepCloneProject/keepCloneApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Save_Note(models.Model):
+    title = models.TextField(max_length=100)
+    note = models.CharField(max_length=1000000)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
