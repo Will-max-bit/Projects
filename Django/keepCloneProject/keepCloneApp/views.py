2ec760dae3fea7707f60ca4a5c65214e948dbe2e
Will-max-bit <whermann4@gmail.com>
2022-11-10 20:03:06 -0800

    got delimiters and backend api working

--- Django/keepCloneProject/keepCloneApp/views.py
+++ Django/keepCloneProject/keepCloneApp/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import *
+
 def index(request):
-    notes = Notes.objects.all()
+    return render(request, 'keepCloneApp/index.html')
+
+def db_notes(request):
+    saved_notes = Notes.objects.all()
     note_data = []
-    for note in notes:
+    for note in saved_notes:
         note_data.append({
             'title': note.title,
             'note_body': note.note_body,
         })
     return JsonResponse({'notes': note_data})
-    return HttpResponse('hello World')
